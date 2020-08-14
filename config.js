f5a9493be2c9cbf5cc7481e45566141ab350b3ac
kidddMonsta <nikolas.yeap@gmail.com>
2020-08-14 11:59:27 +0300

    added config

---
+++ config.js
@@ -0,0 +1,26 @@
+var ADAPTER_LIST = [
+    {
+        "componentType": "bar",
+        "brandName": ["eCharts"]
+    },
+    {
+        "componentType": "grid",
+        "brandName": ["devExpress"]
+    },
+    {
+        "componentType": "line",
+        "brandName": ["eCharts"]
+    },
+    {
+        "componentType": "pie",
+        "brandName": ["devExpress"]
+    },
+    {
+        "componentType": "pivotGrid",
+        "brandName": ["devExpress"]
+    },
+    {
+        "componentType": "textData",
+        "brandName": ["comlogic"]
+    }
+];
